d72ee197c120ebfbb7a6599d4fbaa291dde24c27
Alex <tanko77@rambler.ru>
2023-09-20 09:18:02 +0300

    fix

--- Home1/1.js
+++ Home1/1.js
@@ -16,6 +16,6 @@
 
 let degreesCelsius = prompt('Введите температуру °C :', ['Введите цифры']);
 degreesCelsius = degreesCelsius.replace(/[^0-9]/g,""); //если случайно не убрали надпись введите цифры, оставляем только цифры
-// let degreesFahrenheit = ((9 / 5) * degreesCelsius + 32).toFixed(1); /знак после запятой
-let degreesFahrenheit = Math.round((9 / 5) * degreesCelsius + 32);
+let degreesFahrenheit = ((9 / 5) * degreesCelsius + 32).toFixed(1)*10/10; //знак после запятой
+// let degreesFahrenheit = Math.round((9 / 5) * degreesCelsius + 32);
 alert(`Цельсий: {C}: ${degreesCelsius},` + `  ` + `Фаренгейт: {F}: ${degreesFahrenheit}`);
