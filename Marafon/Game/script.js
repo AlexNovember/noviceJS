7bebb413c9ac389450fd185777f03f85c3029f30
Alex <tanko77@rambler.ru>
2023-09-29 11:04:29 +0300

    add content

---
+++ Marafon/Game/script.js
@@ -0,0 +1,43 @@
+const board = document.querySelector("#board");
+const colors = [];
+for (let i = 0; i < 20; i++) {
+  colors.push(getRandomColors());
+}
+console.log(colors);
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+
+  square.addEventListener("mouseleave", () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px black`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
+
+function getRandomColors() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
