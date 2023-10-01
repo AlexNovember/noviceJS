ec46f0266e9978de045df48890f80aa0daf77e9d
Alex <tanko77@rambler.ru>
2023-10-01 22:02:57 +0300

    add seminar

--- Lesson9/script.js
+++ Lesson9/script.js
@@ -1,25 +1,220 @@
-const product = {
-    name: 'Стол',
-    price: 200
+// const product = {
+//     name: 'Стол',
+//     price: 200,
+//     count: 2,
+//     userRegister: false,
+//     reg: function () {
+//         this.userRegister = true
+//     },
+//     countProduct: function () {
+//         if (this.count > 0) {
+//             console.log("Вы можете купить данный товар");
+//         } else {
+//             console.log('Товар закончился');
+//         }
+//     },
+//     byProduct: function () {
+//         if (this.userRegister) {
+//             console.log('Товар в корзине');
+//         } else {
+//             console.log('Вам нужно авторизоваться');
+//         }
+//     }
+// }
+
+
+// product.byProduct();
+// product.reg();
+// product.byProduct();
+
+// const productNew = {
+//     productImg: 'photo.jpg',
+//     productText: 'Great table'
+// }
+
+// console.log(productNew);
+
+// for (const key in productNew) {
+//     console.log(productNew[key]);
+// }
+
+// console.log(Object.keys(productNew));
+// console.log(Object.values(productNew));
+
+// console.log(product);
+
+// productNew.productLink = 'html'
+// console.log(productNew);
+
+// productNew.productLink = 'aabbbb'
+// console.log(productNew);
+
+// let text = productNew.productLink;
+// console.log(text);
+
+// delete productNew.productLink;
+// console.log(productNew);
+
+// const car = {
+//     make: "Audi",
+//     model: "A5",
+//     year: 2023,
+//     color: "red",
+//     passengers: 2,
+//     power: 249,
+//     odometer: 0,
+// };
+// console.log('Все ключи объекта car');
+
+// for (const key in car) {
+//     console.log(key);
+// }
+// console.log('Все значения объекта car');
+
+// for (const key in car) {
+//     console.log(car[key]);
+// }
+
+
+
+// const group1 = {
+//     "Ivanov":
+//     {
+//         practicePlace: "ldu-1",
+//         practiceTime: 56
+//     },
+//     "Petrov":
+//     {
+//         practicePlace: "kamaz",
+//         practiceTime: 120
+//     },
+//     "Sidorov":
+//     {
+//         practicePlace: "ldu-1",
+//         practiceTime: 148
+//     },
+//     "Belkin":
+//     {
+//         practicePlace: "GosDZU",
+//         practiceTime: 20
+//     },
+//     "Avdeev":
+//     {
+//         practicePlace: "LPK",
+//         practiceTime: 160
+//     }
+// }
+
+// const group1Students = Object.keys(group1);
+// console.log(group1Students);
+// console.log(Object.entries(group1));
+
+const studentsPracticeTime = [
+    {
+        firstName: "Ivanov",
+        practiceTime: 56
+    },
+    {
+        firstName: "Petrov",
+        practiceTime: 120
+    },
+    {
+        firstName: "Sidorov",
+        practiceTime: 148
+    },
+    {
+        firstName: "Belkin",
+        practiceTime: 20
+    }, {
+        firstName: "Avdeev",
+        practiceTime: 160
+    }]; // Мы хотим вывести в таблицу студентов и информацию для каждого, прошел ли он практику уже. Практика будет считаться пройденной, если студент отработал 120 часов или больше. 
+const dataForTable = studentsPracticeTime.map((student) => {
+    if (student.practiceTime < 120) {
+        return {// Мы возвращаем новый объект, болееудобный для вывода. 
+            Student: student.firstName, Practice: "Not passed"
+        };
+    } else {
+        return { Student: student.firstName, Practice: "Passed" };
+    }
+});
+console.table(dataForTable);
+
+const studentsPassedPractice = studentsPracticeTime.filter((student) => {
+    if (student.practiceTime < 120)
+        return false
+    return true
+});
+console.log(studentsPassedPractice);
+
+// Посчитаем сколько всего часов практики отработали студенты. 
+let totalTime = 0;// Объявим переменную для хранениясуммы всех часов. 
+for (let index = 0; index < studentsPracticeTime.length; index++) {
+    totalTime = totalTime + studentsPracticeTime[index].practiceTime;
 }
 
-const productNew = {
-    productImg: 'photo.jpg',
-    productText: 'Great table'
-}
+console.log(totalTime);// 504
 
-console.log(product);
+// Посчитаем сколько всего часов практики отработали студенты. 
+const totalTime2 = studentsPracticeTime.reduce((acc, student) => {// Первое значение - это функция обратного вызова, которая будет получать значение аккумулятора (acc) при каждой итерации; и текущий элемент массива (student). 
+    return acc + student.practiceTime;
+}, 0);// Второй аргумент - это первоначальное значение аккумулятора - 0.
 
-productNew.productLink = 'html'
-console.log(productNew);
-
-productNew.productLink = 'aabbbb'
-console.log(productNew);
-
-let text = productNew.productLink;
-console.log(text);
-
-delete productNew.productLink;
-console.log(productNew);
+console.log(totalTime2);// 504
 
 
+// Проверим, есть ли хоть один студент, которыйпрошел практику. 
+const isSomebodyPassedPractice = studentsPracticeTime.some((student) => {
+    console.log(student.firstName);// Добавим выводстудента, чтобы посмотреть сколько элементов массива будет проитерированно. 
+    return student.practiceTime >= 120;
+}); // "Ivanov"// "Petrov" - итерации остановились на втором элементе массива, т.к. он удовлетворяет нашему условию и метод some дальше итерации не выполняет. 
+console.log(isSomebodyPassedPractice);// true -среди студентов есть те, кто прошел практику.
+
+// Мы хотим найти студента Belkin и посмотреть скольковремени он отработал на практике. 
+const studentBelkin = studentsPracticeTime.find((student) => { return student.firstName === "Belkin"; });
+console.log(studentBelkin.practiceTime);// 20
+
+
+const array = [1, 2, 4, 7, 9, 15];
+console.log(array);
+
+const arrayMap = array.map(item => item * 2);
+console.log(arrayMap);
+
+const arrayFilter = array.filter(item => item >= 10);
+console.log(arrayFilter);
+
+const arraySome = array.some(item => item >= 10);
+console.log(arraySome);
+
+let arrayReduce = array.reduce((acc, el) => acc + el);
+console.log(arrayReduce / array.length);
+
+
+
+// Не очень удобно. Деструктуризация дала нам гораздо более простой и удобный синтаксис: // Сбор данных из объекта. 
+const student = {
+    firstName: "Ivan",
+    lastName: "Petrov",
+    age: 21,
+};
+// const { firstName, lastName, age } = student;
+// Деструктуризация-  мы объявляем все переменные в фигурных скобках, название должно совпадать с нужным нам параметром.
+
+// Сбор данных из массива.
+// const students = ["Ivanov", "Petrov", "Belkin"];
+// const [student1, student2, student3] = students;
+//Деструктуризация - Тут мы указываем имена переменных в квадратных скобках, и в них по порядку будут записаны элементы массива.
+
+// Теперьмыможемобъявлятьвсенужныенампеременныеводномместеиполучатьданныеиз объектов или массивов одной строкой. Деструктуризация также позволяет задавать значения по умолчанию для переменных, на случай если в объекте не окажется такого ключа, который мы запрашиваем, или в массиве в элементе окажется undefined значение. Также при деструктуризации объекта можно переименовать переменные в которые будут сохраняться ключи, если мы например не хотим или не можем использовать переменную с таким же именем как и ключ:
+// Сбор данных из объекта.
+// const student = {
+//     firstName: "Ivan",
+//     lastName: "Petrov",
+//     age: 21,
+// };
+// Деструктурируем значение ключа firstName в переменную studentName. И зададим для возраста значение по умолчанию, равное 20.
+// const { firstName: studentName, lastName, age = 20 } = student; // Сбор данных из массива.
+// const students = ["Ivanov", "Petrov", "Belkin"];
+// const [student1, student2, student3] = students;
+//Тут мы указываем имена переменных в квадратных скобках, и в них по порядку будут записаны элементы массива.
---
+++ Seminar9/script.js
@@ -0,0 +1,123 @@
+"use strict";
+
+// 1. Создайте объект с ключами от 1 до 7, в качестве значений содержащий имена дней недели. Выведите на экран “Вторник”
+// 2. Создайте объект user с ключами 'name', 'surname', ‘age’. Выведите на экран фамилию, имя и возраст через дефис.
+// 3. Добавьте в объект user свойство отчество, которое пользователь должен ввести с клавиатуры
+// 4. Удалите свойство surname
+
+// const daysOfTheWeek = {
+//     1: 'Понедельник',
+//     2: 'Вторник',
+//     3: 'Среда',
+//     4: 'Четверг',
+//     5: 'Пятница',
+//     6: 'Суббота',
+//     7: 'Воскресенье',
+// }
+
+// console.log(daysOfTheWeek[2]);
+
+
+// const user = {
+//     name: 'Alex',
+//     surname: "Ivanov",
+//     age: 26
+// }
+
+// console.log(`${user.name} - ${user.surname} - ${user.age}`);
+
+// const user = {
+//     name: 'Alex',
+//     surname: "Ivanov",
+//     age: 26
+// }
+// user.secondName = prompt('Enter sName');
+// console.log(user);
+
+// delete user.surname;
+// console.log(user);
+
+
+// 1. Даны два массива: первый с названиями дней недели, а второй - с их порядковыми номерами: const arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']; const arr2 = [1, 2, 3, 4, 5, 6, 7]; С помощью цикла создайте объект, ключами которого будут названия дней, а значениями - их номера.
+// 1. const obj = {x: 1, y: 2, z: 3}; Переберите этот объект циклом и возведите каждый элемент этого объекта в квадрат.
+
+// const arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// const arr2 = [1, 2, 3, 4, 5, 6, 7];
+
+// const dasOfWeek = {};
+// for (let i = 0; i < arr2.length; i++) {
+//     daysOfWeek[arr2[i]] = arr1[i];
+// }
+// console.log(dayOfWeek);
+
+// const obj = { x: 1, y: 2, z: 3 }
+
+// for (const key in obj) {
+//     obj[key] = obj[key] ** 2;
+// }
+// console.log(obj);
+
+
+// Найдите сумму элементов приведенного объекта.
+// const obj = {
+//     key1: { key1: 1, key2: 2, key3: 3, },
+//     key2: { key1: 4, key2: 5, key3: 6, },
+//     key3: { key1: 7, key2: 8, key3: 9, },
+// }
+
+// const arrayReduce = Object.values(obj);
+// console.log(arrayReduce.reduce((acc, el) => acc + sum((el), 0)));
+
+// const sum = Object.values(obj).reduce((acc, val) => {
+//     acc += val;
+//     return acc;
+// }, 0);
+// console.log(sum);
+
+// const sum = val =>
+//     val instanceof Object
+//         ? Object.values(val).reduce((acc, n) => acc + sum(n), 0)
+//         : val;
+// console.log(sum(obj));
+
+// 1. Создайте объект riddles
+// 2. Добавьте свойства question, answer
+// 3. создайте метод askQuestion который спрашивает у пользователя вопрос question и сравнивает ответ с answer
+// 4. Если ответил неверно, то в консоль выводится текст: “вы проиграли”
+// 5. * По желанию, создать 2 подсказки, если пользователь ответил неверно
+
+// const riddles = {
+//     question: function name(params) {
+
+//     }
+//     answer: 'Да', 'Нет'
+
+// }
+
+
+const product = {
+    question: 'Зимой и летом одним цветом',
+    answer: 'елка',
+    text1: 'Растет в лесу',
+    askQuestion: function () {
+        let count = 0;
+        do {
+            if (count === 1) {
+                alert("Растет в лесу");
+            } if (count === 2) {
+                alert("Наряжают на Новый Год");
+            }
+
+            const userAnswer = prompt(this.question);
+            if (userAnswer !== this.answer) {
+                alert('Не угадали');
+                count++;
+            } else {
+                alert('Верно');
+                break;
+            }
+        } while (count !== 3);
+    }
+}
+
+product.askQuestion();
