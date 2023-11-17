cd91dbaf6396f5929a71ee1da32303d91af54653
Alex <tanko77@rambler.ru>
2023-11-17 17:47:51 +0300

    add homework

--- DOM/Seminar4/script.js
+++ DOM/Seminar4/script.js
@@ -13,7 +13,11 @@
 //     if (!check.checked) {
 //         error.textContent = 'Необходимо согласиться с условиями';
 //         error.setAttribute('style', 'color: red');
+//         setTimeout(() => {
+//             error.setAttribute('style', 'color: green');
+//         }, "3 second");
 //         e.preventDefault();
+
 //     }
 // });
 
@@ -25,15 +29,49 @@
 
 
 // const form = document.querySelector('form');
+// const check1 = document.querySelector('.tea').textContent;
+// const check2 = document.querySelector('.coffee').textContent;
 
 // form.addEventListener('submit', function (event) {
 //     for (const data of form) {
-//         if (data.checked) {
-//             console.log(data.value)
+
+//         if (check1.checked) {
+//             console.log(element.data.firstElementChild);
+//             // console.log(check1)
 //         }
 //     }
 //     event.preventDefault();
 // });
+
+// const form = document.querySelector('.form');
+// const strTea = form.querySelector('.tea');
+// const strCoffee = form.querySelector('.coffee');
+// const btn = form.querySelector('.btn');
+// const error = form.querySelector('.error');
+// const inputTea = form.querySelector('.inTea');
+// const inputCoffee = form.querySelector('.inCoffee');
+
+// btn.addEventListener('click', errorToProduct)
+
+// function errorToProduct(event) {
+//     event.preventDefault();
+//     if (inputTea.checked) {
+//         abc(error, strTea);
+//     }
+//     else if (inputCoffee.checked) {
+//         abc(error, strCoffee);
+//     }
+//     else {
+//         error.textContent = 'ничего не выбрано. выбирите напиток';
+//     }
+// }
+
+// function addError(str) {
+//     return `${str} закончился`
+// }
+// function abc(error, str) {
+//     error.textContent = addError(str.textContent);
+// }
 
 
 // Создать поле ввода (пароль)
@@ -41,39 +79,82 @@
 // Если пользователь вводит текст “пароль” то поле ввода должно быть подсвечено зеленым цветом
 // Если пароль неверный, у поля ввода появляется красная обводка и текст “пароль неверный”
 
-// const formEl = document.querySelector('form');
-// const inputForm = document.querySelector('input');
-// inputForm.focus();
+const formEl = document.querySelector('form');
+const inputForm = document.querySelector('input');
+inputForm.focus();
 
-// document.addEventListener('submit', (e) => {
-//     let errorText = document.querySelector('p');
-//     inputForm.style.border = '1px solid black';
-//     if (errorText) {
-//         errorText.remove();
+document.addEventListener('submit', (e) => {
+    let errorText = document.querySelector('p');
+    inputForm.style.border = '1px solid black';
+    if (errorText) {
+        errorText.remove();
 
-//     } if (inputForm.value === 'пароль') {
-//         inputForm.style.border = '1px solid green';
-//         console.log(inputForm.value);
-//         inputForm.value = '';
-//     } else {
-//         const errorText = document.createElement('p');
-//         errorText.textContent = 'пароль неверный';
-//         errorText.setAttribute('style', 'color: red');
-//         formEl.append(errorText);
-//         inputForm.style.border = '1px solid red';
-//         console.log(inputForm.value);
-//         e.preventDefault();
-//     }
-// });
+    } if (inputForm.value === 'пароль') {
+        inputForm.style.border = '2px solid green';
+        inputForm.placeholder = 'Пароль правильный';
+        inputForm.value = '';
+    } else {
+        const errorText = document.createElement('p');
+        errorText.textContent = 'пароль неверный';
+        errorText.setAttribute('style', 'color: red');
+        formEl.append(errorText);
+        inputForm.style.border = '2px solid red';
+        console.log(inputForm.value);
+        e.preventDefault();
+    }
+});
+
+
+
 
 
 // Создать поле ввода и под ним заголовок h1 с текстом “Заголовок”
 // При вводе текста в поле ввода необходимо чтобы текст внутри заголовка менятся на введенный в поле ввода
 
-const textArea = document.querySelector('.textarea');
-const textOut = document.querySelector('.text');
+// const textArea = document.querySelector('.textarea');
+// const textOut = document.querySelector('.text');
 
 textArea.addEventListener('keyup', function (e) {
     e.preventDefault();
-    textOut.textContent = textArea.value;
+    textOut.textContent = this.value;
 });
+
+// Event.stopPropagation();
+// Event.stopImmediatePropagation();
+
+
+{/* <form action="#" class="form">
+    <input type="text" name="text" id="myText" class="input_text" />
+    <h1 class="title">Заголовок</h1>
+</form>
+
+
+const inputEl = document.querySelector(".input_text");
+const titleEl = document.querySelector(".title");
+
+document.addEventListener("input", () => {
+    if (inputEl.value === "") {
+        titleEl.textContent = "Заголовок";
+    } else {
+        titleEl.textContent = inputEl.value;
+    }
+});
+
+
+<form action="#" class="form">
+      <input type="text" name="text" id="myText" class="input_text" />
+      <h1 class="title">Заголовок</h1>
+    </form>
+
+    <script>
+      const inputEl = document.querySelector(".input_text");
+      const titleEl = document.querySelector(".title");
+      
+      document.addEventListener("input", () => {
+        if (inputEl.value === "") {
+          titleEl.textContent = "Заголовок";
+        } else {
+          titleEl.textContent = inputEl.value;
+        }
+      });
+    */}
