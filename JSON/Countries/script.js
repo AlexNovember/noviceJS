de69c5d0e20b272d68959e18c13020f2d59ad8c0
Alex <tanko77@rambler.ru>
2023-11-24 13:11:04 +0300

    refactoring

--- JSON/Countries/script.js
+++ JSON/Countries/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const countriesElms = document.querySelector('.countries');
-console.log(countriesElms);
+// console.log(countriesElms);
 
 fetch('https://restcountries.com/v3.1/all')
     .then(function (response) {
@@ -9,73 +9,24 @@ fetch('https://restcountries.com/v3.1/all')
     })
     .then(function (countries) {
         countries.forEach(element => {
-            console.log(element.translations.rus.official);
+            console.log(element.maps.googleMaps);
             if (element.capital !== undefined) {
-                const countryBox = document.createElement('div');
-                countryBox.classList.add('country__box')
-                countriesElms.appendChild(countryBox);
-                const countryName = document.createElement('h3');
-                countryName.classList.add('country__name');
-                countryBox.appendChild(countryName).textContent =
-                    `Страна:\n${element.translations.rus.official}`;
-                const countryFlag = document.createElement('img');
-                countryFlag.classList.add('flag');
-                countryBox.appendChild(countryFlag);
-                countryFlag.src = element.flags.png;
-
-                let newObj = JSON.parse(JSON.stringify(element.capital[0]));
-                const countryCapital = document.createElement('p');
-                countryCapital.classList.add('capital');
-                countryBox.appendChild(countryCapital).textContent =
-                    `Столица: ${newObj}`;
-
-
-                // console.log(`Столица ${newObj}`);
+                countriesElms.insertAdjacentHTML('beforeend', addCountry(element.translations.rus.common, element.capital[0], element.flags.png, element.region, element.maps.googleMaps))
             }
             else {
-                const countryBox = document.createElement('div');
-                countryBox.classList.add('country__box')
-                countriesElms.appendChild(countryBox);
-                const countryName = document.createElement('h3');
-                countryName.classList.add('country__name');
-                countryBox.appendChild(countryName).textContent =
-                    `Страна: ${element.translations.rus.official}`;
-                const countryCapital = document.createElement('p');
-                countryCapital.classList.add('capital');
-                countryBox.appendChild(countryCapital).textContent = 'Нет столицы';
 
+                countriesElms.insertAdjacentHTML('beforeend', addCountry(element.translations.rus.official, 'Нет данных', element.flags.png, element.region, element.maps.googleMaps))
             }
-
-            // console.log(newObj);
-            // console.log(element.region);
-            // console.log(count++);
-
-            // console.log(JSON.stringify(element.capital[0]));
-
-
-            // console.log(houses.region);
         });
-
-
     });
 
-// function addCountry(countries) {
-//     console.log('11');
-// }
-
-// function creatPosts(dataInfo) {
-//     const contentEl = document.querySelector('.content');
-//     dataInfo.forEach((item) => {
-//         contentEl.insertAdjacentHTML('beforeend', getCartCat(item.title, item.body))
-//     })
-// }
-
-function getCartCat(title, body) {
+function addCountry(country, capital, flag, region, maps) {
     return `
-<div class="post">
-    <div class="post__content">
-        <h2>${title}</h2>
-        <p>${body}</p>
-    </div>
+<div class="country__box">    
+        <h2 class='country__name'>${country}</h2>
+        <p class='capital'>Столица: ${capital}</p>
+        <img class='flag' src="${flag}">
+        <p class='region'>${region}</p>
+        <a class="maps" href="${maps}">Посмотреть на карте</a>
 </div>`
 }
