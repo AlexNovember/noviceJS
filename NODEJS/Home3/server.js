df811cb41126f4719c2c0319d611f9adfdca1789
Alex <tanko77@rambler.ru>
2024-01-25 18:44:13 +0300

    fix

--- NODEJS/Home3/server.js
+++ NODEJS/Home3/server.js
@@ -4,10 +4,6 @@ const filePath = path.join(__dirname, 'count.json');
 const fs = require('fs');
 const app = express();
 
-// count1 = 1;
-// count2 = 1;
-// app.use(express.static('static'));
-
 app.use((req, res, next) => {
     console.log('Поступил запрос', req.method, req.url);
     next();
@@ -15,32 +11,22 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
     const jsonData = JSON.parse(fs.readFileSync(filePath));
-    const countTimer = count();
-    res.writeHead(200, {
-        'Content-Type': 'text/html; charset=UTF-8'
-    });
-    res.write('<h1>Корневая страница</h1>')
-    res.write(`Просмотров ${jsonData.count + countTimer}`);
-    res.write('<br>');
-    res.write('<br>');
-    res.write('\n<a href="/about">About</a>');
 
-    addJSON("name", "count", req.url, countTimer);
+    res.send(`<h1>Корневая страница</h1>
+    <p>Просмотров ${jsonData.count + 1}</p>
+    <a href="/about">About</a>`);
+
+    addJSON("name", "count", req.url);
 });
 
 app.get('/about', (req, res) => {
     const jsonData = JSON.parse(fs.readFileSync(filePath));
-    const countTimer = count2();
-    res.writeHead(200, {
-        'Content-Type': 'text/html; charset=UTF-8'
-    });
-    res.write('<h1>About</h1>')
-    res.write(`Просмотров ${jsonData.count + countTimer}`);
-    res.write('<br>');
-    res.write('<br>');
-    res.write('\n<a href="/">Home</a>');
 
-    addJSON("name2", "count2", req.url, countTimer);
+    res.send(`<h1>About</h1>
+        <p>Просмотров ${jsonData.count2 + 1}</p>
+        <a href="/">About</a>`);
+
+    addJSON("name2", "count2", req.url);
 });
 
 const port = 3000;
@@ -49,22 +35,10 @@ app.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
 });
 
-
-function getCounter() {
-    let counter = 1;
-    return function () {
-        return counter++;
-    }
-}
-
-let count = getCounter();
-let count2 = getCounter();
-
-
-function addJSON(key, key2, url, timer) {
+function addJSON(key, key2, url) {
     const jsonData = JSON.parse(fs.readFileSync(filePath));
     jsonData[key] = url;
-    jsonData[key2] = jsonData.count + timer;
+    jsonData[key2] = ++jsonData[key2];
     const counterJSON = JSON.stringify(jsonData, null, 2);
     fs.writeFileSync(filePath, counterJSON);
 }
