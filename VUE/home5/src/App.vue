acb33581eb6797ba297c086cf9a237a6afe9ad64
Alex <tanko77@rambler.ru>
2024-03-12 11:46:22 +0300

    add Home5

---
+++ VUE/home5/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HomeWork/>
+  </div>
+</template>
+
+<script>
+import HomeWork from './components/HomeWork.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomeWork
+  }
+}
+</script>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
